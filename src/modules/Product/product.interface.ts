295b749f033dc06a891cc9ad329d3990d2b00016
Kamrul Hassan <kamrulthedev@gmail.com>
2024-05-22 06:42:05 +0600

    prettier-setup-commit

---
+++ src/modules/Product/product.interface.ts
@@ -0,0 +1,21 @@
+import { Schema, model, connect } from 'mongoose';
+
+export type EVariant = {
+    type: string;
+    value: string;
+}
+
+export type EInventory = {
+    quantity: number;
+    inStock: boolean;
+}
+
+export type EProduct = {
+    name: string;
+    description: string;
+    price: number;
+    category: string;
+    tags: string[];
+    variants: EVariant[];
+    inventory: EInventory;
+};
